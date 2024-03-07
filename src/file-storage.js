8115c4615c44a58de965f0892a8a3ba5d0d62ba0
Aniket Sharma <aniketsharma2468@gmail.com>
2024-03-07 15:57:29 -0500

    updates

--- src/file-storage.js
+++ src/file-storage.js
@@ -2,6 +2,7 @@
 const { BlobServiceClient, StorageSharedKeyCredential } = require('@azure/storage-blob');
 
 async function getTheImage(accountName, accessKey, imagePath) {
+    console.log(accountName, accessKey, imagePath);
     // Use the StorageSharedKeyCredential with your storage account and access key
     const blobServiceClient = new BlobServiceClient(
         `https://${accountName}.blob.core.windows.net`,
